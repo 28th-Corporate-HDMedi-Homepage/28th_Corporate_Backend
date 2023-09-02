1cca61390ab24f85d097d1939c5083474abea515
eckrin <bsh51515@naver.com>
2023-09-02 23:14:39 +0900

    init: project initialize

---
+++ src/main/java/com/kusitms/hdmedi/HdmediApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.hdmedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdmediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdmediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusitms/hdmedi/HdmediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.hdmedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdmediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
