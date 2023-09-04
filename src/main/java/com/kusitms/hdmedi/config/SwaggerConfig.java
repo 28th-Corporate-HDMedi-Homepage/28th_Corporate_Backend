ad2db34620f1eb8505f6f1d17ae14f89d713f558
eckrin <bsh51515@naver.com>
2023-09-04 23:33:47 +0900

    feat: swagger(springdoc) 추가

---
+++ src/main/java/com/kusitms/hdmedi/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.kusitms.hdmedi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//http://localhost:8080/swagger-ui/index.html
+@Configuration
+public class SwaggerConfig {
+
+    private final String TITLE = "HDMedi A조 Api";
+    private final String DESCRIPTION = "KUSITMS x HDMedi";
+    private final String VERSION = "V0.1";
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(TITLE)
+                        .description(DESCRIPTION)
+                        .version(VERSION)
+                );
+    }
+}
