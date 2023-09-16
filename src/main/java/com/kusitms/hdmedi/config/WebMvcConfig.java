8a0e00246440bfe8886d8b54f946eb890f7646c5
MyunghyunNero <mh1kim0910@gmail.com>
2023-09-16 21:12:57 +0900

    re 수정

--- src/main/java/com/kusitms/hdmedi/config/WebMvcConfig.java
+++ src/main/java/com/kusitms/hdmedi/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry){
